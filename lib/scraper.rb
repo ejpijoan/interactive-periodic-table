59d9e4f2a9e3c5e5ef859118a83da4c002b76ca0
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-02 20:14:35 -0800

    creates collect_data method in Scraper Class to collect additional data about an element.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,8 +29,14 @@ class Scraper
 
     def collect_data(link = "https://www.webelements.com/hydrogen/")
         page = self.open_page("https://www.webelements.com/hydrogen/")
-        #
-
+        properties = page.css('ul.ul_facts_table').text.split(/\n/).map{|item| item.strip}
+        properties_hash = {
+        :mass => properties.drop(1).find{|item| item.include? "mass"}.gsub(/(Relative atomic mass)|\(|[Ar]|\)|\:/,"").strip,
+        :standard_state => properties.drop(1).find{|item| item.include? "state"}.gsub(/(Standard state:)/, "").strip,
+        :appearance => properties.drop(1).find{|item| item.include? "Appearance"}.gsub(/(Appearance:)/, "").strip,
+        :classification => properties.drop(1).find{|item| item.include? "Class"}.gsub(/(Classification:)/, "").strip,
+        :facts => page.css('p.p_first').css('p').text.split(/\n/).join.strip
+        }
     end
 
     binding.pry
