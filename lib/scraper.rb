ba2d034c3f05c42685669374dff826bed6d4566e
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-02 17:56:45 -0800

    adds initialize method and makes open_page method more abstract.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,15 +3,20 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
-    def open_page
-        Nokogiri::HTML(open('https://www.webelements.com/nexus/list-of-elements-by-atomic-number/'))
+    attr_accessor :url
+
+    def initialize(url)
+        @url = 'https://www.webelements.com/nexus/list-of-elements-by-atomic-number/'
+    end
+
+    def open_page(url)
+        Nokogiri::HTML(open(url))
     end
 
     def collect_elements
-        #page = self.open_page
-        page = Nokogiri::HTML(open('https://www.webelements.com/nexus/list-of-elements-by-atomic-number/'))
+        page = self.open_page(self.url)
         elements_list = page.css('div.kcite-section').css('tr').drop(1)
-        elements_array = elements_list.collect do |element| 
+        elements_list.collect do |element| 
             array = element.text.split(/\n/).drop(1)
             hash = { 
                 :name => array[0],
@@ -20,7 +25,12 @@ class Scraper
                 :link => element.css('td a').attribute('href').value
             }
         end
-        
+    end
+
+    def collect_data(link = "https://www.webelements.com/hydrogen/")
+        page = self.open_page("https://www.webelements.com/hydrogen/")
+        #
+
     end
 
     binding.pry
