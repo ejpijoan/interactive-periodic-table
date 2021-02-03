44ef2203bc1006b8f95373603d02545fabb60fb4
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-02 17:25:04 -0800

    creates collect_elements method in Scraper class to generate a nested array of elements

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,10 +3,25 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
-
     def open_page
-        periodic_table = Nokogiri::HTML(open('https://pubchem.ncbi.nlm.nih.gov/periodic-table/'))
+        Nokogiri::HTML(open('https://www.webelements.com/nexus/list-of-elements-by-atomic-number/'))
     end
-binding.pry
 
+    def collect_elements
+        #page = self.open_page
+        page = Nokogiri::HTML(open('https://www.webelements.com/nexus/list-of-elements-by-atomic-number/'))
+        elements_list = page.css('div.kcite-section').css('tr').drop(1)
+        elements_array = elements_list.collect do |element| 
+            array = element.text.split(/\n/).drop(1)
+            hash = { 
+                :name => array[0],
+                :symbol => array[1],
+                :number => array [2], 
+                :link => element.css('td a').attribute('href').value
+            }
+        end
+        
+    end
+
+    binding.pry
 end
