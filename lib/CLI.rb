072fe26672e4cf514ef10293f38d51c8f629e489
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-03 16:51:50 -0800

    separates opening greeting into it's own method, welcome, in CLI class.

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,8 +2,11 @@ require 'pry'
 
 class CLI
 
-    def collect_element
+    def welcome
         puts "Hello, welcome to the Periodic Table of the Elements."
+    end
+
+    def collect_element
         puts "Please choose an element to learn more about it by typing the name, atomic number, or atomic symbol and pressing enter. Alternatly you may choose from a list of the ten most common elements by entering 'top ten'."
         input = gets
         if name
