915c84d03b3d393fabbf4e72050b7f4b26bf5aad
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-03 16:47:29 -0800

    defines collect_element method in CLI class

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,6 +1,19 @@
 require 'pry'
 
 class CLI
-     
+
+    def collect_element
+        puts "Hello, welcome to the Periodic Table of the Elements."
+        puts "Please choose an element to learn more about it by typing the name, atomic number, or atomic symbol and pressing enter. Alternatly you may choose from a list of the ten most common elements by entering 'top ten'."
+        input = gets
+        if name
+        elsif number
+        elsif symbol
+        elsif top ten
+        else
+            puts "The element you have entered is not valid."
+            self.collect_element 
+        end
+    end
 end
 
