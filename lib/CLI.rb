f8f2ed8303ed87d81b5f6fb189609fab15a543f9
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-04 09:42:35 -0800

    adds valid element? method to CLI class

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -10,17 +10,27 @@ class CLI
     end
 
     def collect_element
-        puts "Please choose an element to learn more about it by typing the name, atomic number, or atomic symbol and pressing enter. Alternatly you may choose from a list of the ten most common elements by entering 'top ten'."
+        puts "Please choose an element to learn more about it by typing the name, atomic number, or atomic symbol and pressing enter. Alternately you may choose from a list of the ten most common elements by entering 'top ten'."
         input = gets
         if Element.all.find{|element| element.name == input || element.number == input || element.symbol == input}
         element = Element.all.find{|element| element.name == input || element.number == input || element.symbol == input}
         self.collect_info(element)
         elsif input == "top ten"
+            puts "Ten Most Common Elements on Earth:"
             self.top_ten.each_with_index {|element, index| puts "#{index+1}. #{element}"}
         else
             puts "The element you have entered is not valid."
             self.collect_element 
         end
     end
+
+    def valid_element?(input)
+        if Element.all.find{|element| element.name == input || element.number == input || element.symbol == input}
+            true
+        else
+            false 
+        end
+    end
+
 end
 
