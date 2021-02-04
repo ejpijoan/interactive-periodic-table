d723f20d86b8746c18f4c657b4c1f00245dcac44
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-04 12:05:00 -0800

    defines new collect_elements method in CLI contains code to create element instances.

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -18,9 +18,12 @@ class CLI
         puts "Hello, welcome to the Periodic Table of the Elements."
     end
 
-    def self.choose_element
+    def collect_elements
         array = Scraper.collect_elements
         Elements.create_from_scraped_data(array)
+    end
+
+    def self.choose_element
         puts "Please choose an element to learn more about it by typing the name, atomic number, or atomic symbol and pressing enter. Alternately you may choose from a list of the ten most common elements by entering 'top ten'."
         input = gets
         if self.valid_element?(input)
